5a3743e011181bcd118e8088b5d26ae4b21e65bf
Augusto Achá <augustoacha177@gmail.com>
2025-07-04 18:02:18 -0300

    se actualizó el area Observaciones, se sacó 3 columnas para que quepan en el contenedor

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 let titulo = document.getElementsByTagName('title');
 console.log(titulo[0].textContent='Clases de Música');
+
+/**Cards */
+let cards = document.querySelectorAll('#cards');
+console.log(cards.innerHtml);
+
+
+   
