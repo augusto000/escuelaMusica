431117b94000e04cf15bbb8248a3f65f351fdf72
Augusto Achá <augustoacha177@gmail.com>
2025-07-04 07:21:10 -0300

    comenzando script.js agregando imagenes usando for

--- scripts.js
+++ scripts.js
@@ -1,2 +1,12 @@
 let titulo = document.getElementsByTagName('title');
 console.log(titulo[0].textContent='Clases de Música');
+
+let cards_= document.querySelectorAll("cards");
+
+for (let item in cards_){
+    console.log('soy '+ item);
+      if (item === 'img'){
+        
+        console.log(`soy :${cards_[item]}`);
+    }
+}
