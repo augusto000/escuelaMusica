d7629813a908dc4c3bc232a63261af8a8e18a765
Augusto Achá <augustoacha177@gmail.com>
2025-07-07 16:04:29 -0300

    actualizacion sobre las cards

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
 let titulo = document.getElementsByTagName('title');
-console.log(titulo[0].textContent='Clases de Música');
+console.log(titulo[0].textContent='CM');
 
 /**Cards */
-let cards = document.querySelectorAll('#cards');
-console.log(cards.innerHtml);
+let divs = document.querySelector('#cards');
+let nuevaImg = document.createElement('img');
+nuevaImg.src='media/6.jpeg';
+nuevaImg.alt='nuevaImagen';
 
-
-   
+if (divs.insertAdjacentElement('afterend',nuevaImg)){
+   console.log("La nueva imágen ",nuevaImg);
+   divs.appendChild(nuevaImg);
+}
+console.log(typeof(NodeList));
+/**Recorro el NodeList */
+divs.forEach(element, indice, divs => {
+    console.log(`${element} : en la posición ${indice + 1}`);
+});
