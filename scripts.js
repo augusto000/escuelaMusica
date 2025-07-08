07adc99e6aea8a9dc1716d0025e6ff8ee724b8f1
Augusto Achá <augustoacha177@gmail.com>
2025-07-08 18:04:23 -0300

    actualización se agrega una nueva imagen a travez del DOM

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
 let titulo = document.getElementsByTagName('title');
 console.log(titulo[0].textContent='CM');
 
-let divs = document.querySelectorAll("#cards");
-//console.log(divs[0])
-let nuevaImg = document.createElement('img');
-    nuevaImg.src="media/6.jpeg";
-    nuevaImg.alt="Nueva imagen del Curso de Guitarra";
-    nuevaImg.title="Imagen del Curso de Guitarra";
-divs[0].appendChild(nuevaImg);
+let divs = document.getElementById("#cards");
+
+let nuevoElement = document.createElement('img');
+divs.forEach(item =>{
+    let ultimoElement = item.lastElementChild;
+    if (ultimoElement){
+        //console.log(ultimoElement);
+        nuevoElement.src="media/6.jpeg";
+        nuevoElement.alt="Nueva imagen de curso";
+        nuevoElement.title="Nueva image del curso";
+        divs.appendChild(nuevoElement);
+    }
+    else{
+        console.log("No se encontro el último elemento");
+    }
+
+    
+});
+
+
 
