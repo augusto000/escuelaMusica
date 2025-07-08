4f17ec1b70c61adda16b18caa54f2a1377a91b01
Augusto Achá <augustoacha177@gmail.com>
2025-07-08 12:39:27 -0300

    actualización se agrega una nueva imagen a travez del DOM

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ let titulo = document.getElementsByTagName('title');
 console.log(titulo[0].textContent='CM');
 
 let divs = document.querySelectorAll("#cards");
-console.log(divs[0])
+//console.log(divs[0])
 let nuevaImg = document.createElement('img');
     nuevaImg.src="media/6.jpeg";
     nuevaImg.alt="Nueva imagen del Curso de Guitarra";
