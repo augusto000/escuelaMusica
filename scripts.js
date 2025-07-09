0a7af83c06211b38a5194a6d24318f02bab18fd7
Augusto Achá <augustoacha177@gmail.com>
2025-07-09 13:16:49 -0300

    actualizando las img de los cursos en las cards

--- scripts.js
+++ scripts.js
@@ -3,12 +3,16 @@ console.log(titulo[0].textContent='CM');
 
 let divs = document.getElementById("cards");
 
-for(let i=6; i< 13; i++){
+for(let i=1; i< 13; i++){
+      if ((i===8) || (i ===12)){
+            console.log();
+      }
+      else{
       let nuevoElement = document.createElement('img');
       nuevoElement.src = "media/"+`${i}`+".jpeg";
       console.log(nuevoElement);
       divs.appendChild(nuevoElement);
+      }
 }
 console.log(divs);
 
-divs.forE
