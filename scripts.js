4b2f99b846f5e6c76040822f3a112641f0787536
Augusto Achá <augustoacha177@gmail.com>
2025-07-09 13:24:51 -0300

    actualizando las img de los cursos en las cards

--- scripts.js
+++ scripts.js
@@ -1,9 +1,10 @@
 let titulo = document.getElementsByTagName('title');
-console.log(titulo[0].textContent='CM');
+console.log(titulo[0].textContent='Clases de Música');
 
 let divs = document.getElementById("cards");
 
 for(let i=1; i< 13; i++){
+      //acomodar los tamañ0s  de esas fotos la 8 y la 12
       if ((i===8) || (i ===12)){
             console.log();
       }
