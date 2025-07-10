3fef5a65e9455e1f1903d2ee7f537f133c9c9f83
Augusto Achá <augustoacha177@gmail.com>
2025-07-10 16:19:42 -0300

    se agrega el event.preventDefault a la funcion muestro()

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ let item = divss[0];
 //console.log('soy :'+item);
 
 let imgs = document.getElementById("cards");
-for(let i=1; i< 13; i++){
+for(let i=2; i< 13; i++){
       //acomodar los tamañ0s  de esas fotos la 8 y la 12
       if ((i===8) || (i ===12)){
             console.log();
@@ -22,10 +22,16 @@ for(let i=1; i< 13; i++){
 let boton = document.getElementById('mibtn');
 
 let cajaCarrito = document.getElementById('cajaFlotante');
+//apunto al org/dst del nombre
+let nombreOrg = document.querySelector('#nombreOrg').value;
+prompt(nombreOrg);
+let nombreDst = document.getElementById('nombreDst');
 
-function muestro(event){
+function muestro(event, nombreOrg, nombreDst){
       event.preventDefault();      
-    
+      nombreDst.textContent = nombreOrg;
     }
 
-boton.addEventListener("click", muestro);
+boton.addEventListener("click", function(event){
+      muestro(event, nombreOrg, nombreDst);
+});
