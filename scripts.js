8a5f594d8980c2fc12a7529c4ab26dabe3ef70b5
Augusto Achá <augustoacha177@gmail.com>
2025-07-11 13:01:55 -0300

    se actualiza codigo de verificar carrito y se modulariza el código

--- scripts.js
+++ scripts.js
@@ -1,10 +1,6 @@
 let titulo = document.getElementsByTagName('title');
 console.log(titulo[0].textContent='CMú');
 
-//let divss = document.getElementById('cards');
-//let item = divss[0];
-//console.log('soy :'+item);
-
 let imgs = document.getElementById("cards");
 for(let i=2; i< 13; i++){
       //acomodar los tamañ0s  de esas fotos la 8 y la 12
@@ -25,33 +21,115 @@ let boton = document.getElementById('mibtn');
 //referenciio la caja flotante
 //let cajaCarrito = document.getElementById('cajaFlotante');
 
-function muestro(event){
+function campoNombre(event){                        
                         //referencio para limpiar el inputText  
-                        let refInputText = document.getElementById('nombreOrg');
+                       // let refInputText = document.getElementById('nombreOrg');
                         //Tomo el valor que contiene a travez de .value
                         let nombreOrg = document.getElementById('nombreOrg').value;
+                        return nombreOrg;
                   //si esta vacía la cadena.
                   if(nombreOrg.trim() !== ''){                        
                         //apunto al campo destino
                         let nombreDst = document.getElementById('nombreDst');
                         nombreDst.innerText = nombreOrg;
-                        refInputText.value = " ";
+                        //refInputText.value = " ";
                         //que no recargue la página
-                        event.preventDefault();
-                        alert("Datos guardados!");
+                        //event.preventDefault();
+                        noRecargarPagina(event);                        
+                  }  
+                  else{
+                        alert(`Atención: debe escribir todos sus datos.`);
+                        //limpiar de todas maneras.
+                        //limpiarCampos();
+                        //No recargar la página                        
+                        noRecargarPagina(event);
+                  }                         
+    }
+    function campoApellido(event){                        
+                        //referencio para limpiar el inputText 
+                        let refApeOrg = document.getElementById('apellidoOrg');                       
+                        //Tomo el valor que contiene a travez de .value
+                        let apellidoOrg = document.getElementById('apellidoOrg').value;
+                        return apellidoOrg;
+                  //si esta vacía la cadena.
+                  if(apellidoOrg.trim() !== ''){                        
+                        //apunto al campo destino
+                        let apellidoDst = document.getElementById('apeDst');
+                        //copiar valor
+                        apellidoDst.innerText = apellidoOrg;
+                        //refInputText.value = " ";
+                        //que no recargue la página
+                        //event.preventDefault();
+                        //alert("Datos guardados!");
+                        noRecargarPagina(event);                        
+                  }  
+                  else{
+                        alert(`Atención: debe escribir todos sus datos.`);
+                        //limpiar de todas maneras.
+                        //limpiarCampos();
+                        //No recargar la página                        
+                        noRecargarPagina(event);
+                  }                         
+    }
+    function noRecargarPagina(event){
+          event.preventDefault();
+    }
+    function limpiarCampos(){
+                        //referencio para limpiar el inputText  
+                        let refInputTextNom = document.getElementById('nombreOrg');
+                        refInputTextNom.value = " ";
+                        let refInputTextApe =document.getElementById('apellidoOrg');
+                        refInputTextApe.value = " ";
+    }
+    function cajaCarrito(){
                         //hago aparecer la caja del carrito
                         let cajaFlotante = document.getElementById('cajaFlotante');
                         cajaFlotante.style.display='flex';
                         cajaFlotante.style.justifyContent='center';
                         cajaFlotante.style.alignItems='top';
                         cajaFlotante.style.flexWrap='wrap';
-                  }  
+    }
+    function verificarDatos(event, nombreOrg, apellidoOrg){
+                        //si esta vacía la cadena.
+                  if(nombreOrg.trim() !== ''){                        
+                        //apunto al campo destino
+                        let nombreDst = document.getElementById('nombreDst');
+                        nombreDst.innerText = nombreOrg;
+                        //refInputText.value = " ";
+                        //que no recargue la página
+                        //event.preventDefault();
+                        noRecargarPagina(event);
+                  }
                   else{
-                        alert(`Atención: debe escribir su nombre`);
+                        alert(`Atención: debe escribir todos sus datos.`);
                         //limpiar de todas maneras.
-                        refInputText.value = " ";
-                  }       
+                        //limpiarCampos();
+                        //No recargar la página                        
+                        noRecargarPagina(event);
+                  }
+                  if(apellidoOrg.trim() !== ''){                        
+                        //apunto al campo destino
+                        let apellidoDst = document.getElementById('apeDst');
+                        //copiar valor
+                        apellidoDst.innerText = apellidoOrg;
+                        //refInputText.value = " ";
+                        //que no recargue la página
+                        //event.preventDefault();
+                        //alert("Datos guardados!");
+                        noRecargarPagina(event);                 
+                  }
+                  else{
+                        alert(`Atención: debe escribir todos sus datos.`);
+                        //limpiar de todas maneras.
+                        //limpiarCampos();
+                        //No recargar la página                        
+                        noRecargarPagina(event);
+                  }
     }
 boton.addEventListener("click", function(event){
-         muestro(event);
+         let nombreOrg = campoNombre(event);
+         let apellidoOrg = campoApellido(event);
+         verificarDatos(event, nombreOrg, apellidoOrg);
+         //cajaCarrito(event)
+         limpiarCampos();
 });
